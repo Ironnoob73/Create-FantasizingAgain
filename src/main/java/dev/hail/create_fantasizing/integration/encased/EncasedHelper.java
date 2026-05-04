97cbd5acbe4a8c2092933000404d11e5f6bc5cbd
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2026-05-04 10:47:32 +0800

    Revert "Shadow steel tunnel"
    This reverts commit 4714caa0ba4c53f54de757825e376e526da062c0.

--- src/main/java/dev/hail/create_fantasizing/integration/encased/EncasedHelper.java
+++
@@ -1,8 +0,0 @@
-package dev.hail.create_fantasizing.integration.encased;
-
-import net.neoforged.fml.loading.LoadingModList;
-
-public class EncasedHelper {
-    public static final String MOD_ID = "createcasing";
-    public static final boolean IS_LOADED = LoadingModList.get().getModFileById(MOD_ID) != null;
-}
