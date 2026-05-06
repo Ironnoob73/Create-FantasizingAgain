e9cd98e3ac0dd71ef6b7ec39ac9b2891c652bda2
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2026-05-06 16:53:58 +0800

    JEI Support

---
+++ src/main/java/dev/hail/create_fantasizing/integration/jei/FantasizingJEI.java
@@ -0,0 +1,94 @@
+package dev.hail.create_fantasizing.integration.jei;
+
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllItems;
+import com.simibubi.create.compat.jei.ToolboxColoringRecipeMaker;
+import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
+import dev.hail.create_fantasizing.FantasizingMod;
+import dev.hail.create_fantasizing.block.CFABlocks;
+import dev.hail.create_fantasizing.block.chromatic_tunnel.ShadowPlatingRecipe;
+import dev.hail.create_fantasizing.data.CFARecipeTypes;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.runtime.IIngredientManager;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeInput;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
+import java.util.List;
+
+@JeiPlugin
+@SuppressWarnings("unused")
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class FantasizingJEI implements IModPlugin {
+    private static final ResourceLocation ID = FantasizingMod.resourceLocation("jei_plugin");
+
+    private final List<CreateRecipeCategory<?>> allCategories = new ArrayList<>();
+    private IIngredientManager ingredientManager;
+
+    private void loadCategories() {
+        allCategories.clear();
+
+        CreateRecipeCategory<?>
+                shadow_plating = builder(ShadowPlatingRecipe.class)
+                .addTypedRecipes(CFARecipeTypes.SHADOW_PLATING)
+                .catalyst(AllBlocks.MECHANICAL_PRESS::get)
+                .doubleItemIcon(CFABlocks.SHADOW_STEEL_TUNNEL.get(), AllItems.SHADOW_STEEL.get())
+                .emptyBackground(177, 70)
+                .build("shadow_plating", ShadowPlatingCategory::new);
+    }
+
+    private <T extends Recipe<? extends RecipeInput>> FantasizingJEI.CategoryBuilder<T> builder(Class<T> recipeClass) {
+        return new FantasizingJEI.CategoryBuilder<>(recipeClass);
+    }
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        ingredientManager = registration.getIngredientManager();
+        allCategories.forEach(c -> c.registerRecipes(registration));
+        registration.addRecipes(RecipeTypes.CRAFTING, ToolboxColoringRecipeMaker.createRecipes().toList());
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        allCategories.forEach(c -> c.registerCatalysts(registration));
+    }
+
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        loadCategories();
+        registration.addRecipeCategories(allCategories.toArray(IRecipeCategory[]::new));
+    }
+
+    private class CategoryBuilder<T extends Recipe<?>> extends CreateRecipeCategory.Builder<T> {
+        public CategoryBuilder(Class<? extends T> recipeClass) {
+            super(recipeClass);
+        }
+
+        @Override
+        public CreateRecipeCategory<T> build(ResourceLocation id, CreateRecipeCategory.Factory<T> factory) {
+            CreateRecipeCategory<T> category = super.build(id, factory);
+            allCategories.add(category);
+            return category;
+        }
+        @Override
+        public CreateRecipeCategory<T> build(String name, CreateRecipeCategory.Factory<T> factory) {
+            return build(FantasizingMod.resourceLocation(name), factory);
+        }
+    }
+}
---
+++ src/main/java/dev/hail/create_fantasizing/integration/jei/ShadowPlatingAnimation.java
@@ -0,0 +1,68 @@
+package dev.hail.create_fantasizing.integration.jei;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.AllPartialModels;
+import com.simibubi.create.compat.jei.category.animations.AnimatedKinetics;
+import com.simibubi.create.content.kinetics.belt.BeltBlock;
+import com.simibubi.create.content.kinetics.belt.BeltPart;
+import com.simibubi.create.content.kinetics.belt.BeltRenderer;
+import dev.engine_room.flywheel.lib.model.baked.PartialModel;
+import dev.hail.create_fantasizing.block.CFABlocks;
+import net.createmod.catnip.animation.AnimationTickHolder;
+import net.createmod.catnip.render.CachedBuffers;
+import net.createmod.catnip.render.SpriteShiftEntry;
+import net.createmod.catnip.render.SuperByteBuffer;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.RenderType;
+
+public class ShadowPlatingAnimation extends AnimatedKinetics {
+
+    PartialModel beltPartial = AllPartialModels.BELT_MIDDLE;
+
+    @Override
+    public void draw(GuiGraphics guiGraphics, int xOffset, int yOffset) {
+        PoseStack matrixStack = guiGraphics.pose();
+        matrixStack.pushPose();
+        matrixStack.translate(xOffset, yOffset + 13, 200);
+        matrixStack.mulPose(Axis.XP.rotationDegrees(-15.5f));
+        matrixStack.mulPose(Axis.YP.rotationDegrees(22.5f));
+
+        // Don't know how to get the animation work...
+        SuperByteBuffer beltBuffer = CachedBuffers.partial(beltPartial, AllBlocks.BELT.getDefaultState().setValue(BeltBlock.PART, BeltPart.MIDDLE));
+        SpriteShiftEntry spriteShift = BeltRenderer.getSpriteShiftEntry(null, false, false);
+        float spriteSize = spriteShift.getTarget().getV1() - spriteShift.getTarget().getV0();
+        double scroll = AnimationTickHolder.getRenderTime() / (31.5 * 16);
+        scroll = scroll - Math.floor(scroll);
+        scroll = scroll * spriteSize * 0.5;
+        beltBuffer.shiftUVScrolling(spriteShift, (float) scroll);
+        PoseStack localTransforms = new PoseStack();
+        VertexConsumer vb = guiGraphics.bufferSource().getBuffer(RenderType.solid());
+        beltBuffer.transform(localTransforms).renderInto(guiGraphics.pose(), vb);
+
+        blockElement(beltPartial)
+                .rotateBlock(0, 90, 0)
+                .atLocal(0, 1, 0)
+                .scale(24)
+                .render(guiGraphics);
+        blockElement(beltPartial)
+                .rotateBlock(0, 90, 0)
+                .atLocal(-1, 1, 0)
+                .scale(24)
+                .render(guiGraphics);
+        blockElement(beltPartial)
+                .rotateBlock(0, 90, 0)
+                .atLocal(1, 1, 0)
+                .scale(24)
+                .render(guiGraphics);
+
+        blockElement(CFABlocks.SHADOW_STEEL_TUNNEL.getDefaultState())
+                .atLocal(0, 0, 0)
+                .scale(24)
+                .render(guiGraphics);
+
+        matrixStack.popPose();
+    }
+}
